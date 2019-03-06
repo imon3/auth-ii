21830eb96e97aece8991a93271f4f7b5785efa52
Imon Ovbude <imonovbude@gmail.com>
2019-03-06 15:22:22 -0800

    created login post request

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,6 +5,9 @@ const jwt = require('jsonwebtoken');
 const secret = require('../secret/secret');
 const Users = require('../users/users-model');
 
+// TOKEN GENERATOR
+
+
 // POST REQUEST TO REGISTER A USER
 router.post('/register', (req, res) => {
     const user = req.body;
@@ -15,6 +18,30 @@ router.post('/register', (req, res) => {
     Users.add(user)
         .then(savedUser => {
             res.status(201).json(savedUser)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+// POST REQUEST TO LOGIN USER
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    Users.findUserBy({ username })
+        .first()
+        .then(user => {
+            if (user && bycryptjs.compareSync(password, user.password)) {
+                const token = generateToken(user);
+                res.status(200).json({
+                    message: `Welcome ${user.username}`,
+                    token
+                })
+            } else {
+                res.status(401).json({
+                    message: 'Credentials are invalid'
+                })
+            }
         })
         .catch(err => {
             res.status(500).json(err)
