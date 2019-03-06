3574ceb0b16542eb33019d9b208644b20e12f9bf
Imon Ovbude <imonovbude@gmail.com>
2019-03-06 15:10:35 -0800

    created register user post request

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,4 +5,20 @@ const jwt = require('jsonwebtoken');
 const secret = require('../secret/secret');
 const Users = require('../users/users-model');
 
+// POST REQUEST TO REGISTER A USER
+router.post('/register', (req, res) => {
+    const user = req.body;
+
+    const hash = bcryptjs.hashSync(user.password, 12)
+    user.password = hash;
+
+    Users.add(user)
+        .then(savedUser => {
+            res.status(201).json(savedUser)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
 module.exports = router;
--- users/users-router.js
+++ users/users-router.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const Users = require('./users-model');
 const restricted = require('../auth/restricted-middleware');
 
+// GET REUEST FOR ALL USERS
 router.get('/', restricted, (req, res) => {
     Users.find()
         .then(users => {
