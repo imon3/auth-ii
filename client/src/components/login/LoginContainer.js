4cc2c51b81c7ba8ba2897f903e4ce209c0efef96
Imon Ovbude <imonovbude@gmail.com>
2019-03-07 13:18:34 -0800

    created login page

---
+++ client/src/components/login/LoginContainer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios';
+
+class Login extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleInput = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        axios.post('http://localhost:5000/api/auth/login', this.state)
+            .then(res => {
+                console.log(res)
+                localStorage.setitem('jwt', res.data.token)
+            })
+            .catch(err => console.log(err))
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Login</h2>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input placeholder='username' type='text' name='username' value={this.state.username} onChange={this.handleInput} />
+                    <input placeholder='password' type='text' name='password' value={this.state.password} onChange={this.handleInput} />
+                    <button type='submit'>Log-In</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login;
